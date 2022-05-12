fa28a314d5d6af03cbe97c908e5473fcdaf521b1
ArkadiBagratyan <aka.bagratyan@mail.ru>
2022-05-12 23:37:55 +0300

    Homework

---
+++ src/homework/lesson7/Figure.java
@@ -0,0 +1,19 @@
+package homework.lesson7;
+
+public class Figure {
+    public static void main(String[] args) {
+        for (int i = 4; i > 0 ; i--) {
+            for (int j = 8; j > i; j--) {
+                System.out.print("  "); }
+            for (int j = 1; j <=i ; j++) {
+
+                System.out.print("* ");}
+            System.out.println();
+
+            }
+
+
+                
+            }
+
+            }
